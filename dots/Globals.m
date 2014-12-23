bfbfe234dc02d13b7f0e71afe51a8a969c84cb67
arriceclaimer <darklord_nekiy1@ukr.net>
2014-12-24 02:22:39 +0400

    initial changes
    array of buttons, bonus info

---
+++ dots/Globals.m
@@ -0,0 +1,13 @@
+//
+//  Globals.m
+//  dots
+//
+//  Created by Admin on 24.12.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import "Globals.h"
+
+@implementation Globals
+
+@end
--- dots/dots/ViewController.m
+++ dots/dots/ViewController.m
@@ -14,9 +14,24 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    NSMutableArray *buttons = [NSMutableArray new];
+    int cellWidth = [[UIScreen mainScreen] bounds].size.height / 13;
+    NSLog(@"%d", cellWidth);
+    CGPoint buttonStartPoint = CGPointMake([[UIScreen mainScreen] bounds].size.width * 2 / 17,
+                                            [[UIScreen mainScreen] bounds].size.height * 1 / 13);
+    for (int i = 0; i < FIELD_SIZE*FIELD_SIZE; i++) {
+        buttons[i] = [[UIButton alloc] initWithFrame:CGRectMake(buttonStartPoint.x + i % FIELD_SIZE * cellWidth,
+                                                                buttonStartPoint.y + i / FIELD_SIZE * cellWidth,
+                                                                cellWidth, cellWidth)];
+        //[buttons[i] setType:UIButtonTypeCustom];
+        [buttons[i] setTitle:[NSString stringWithFormat:@"%d", i]];
+        [buttons[i] setBackgroundColor:[UIColor redColor]];
+        [self.view addSubview:buttons[i]];
+    }
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
